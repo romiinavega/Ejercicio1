c99ddedc24f2d2b3377867e12a23ddf240530a9c
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-15 07:36:42 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Enemigo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1
+{
+    class Enemigo
+    {
+        public int posicionX { get; set; }
+        public int posicionY { get; set; }
+
+        private int _puntos;
+        public int Puntos
+        {
+            get
+            {
+                return _puntos;
+            }
+
+            set
+            {
+                if (value >= 100)
+                {
+                    _puntos = value;
+                }
+                else
+                {
+                    throw new Exception("El puntaje debe ser mayor que 100");
+                }
+
+            }
+        }
+        public void moverse()
+        {
+
+        }
+        public void disparar ()
+        {
+
+        }
+        public void explotar ()
+        {
+
+        }
+    }
+    
+}
